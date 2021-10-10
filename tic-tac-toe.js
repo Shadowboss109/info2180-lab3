fb47de5e0f1c9ecbfec1d9215fc2e740c6caf5fe
Theodore <theodorebennett3@gmail.com>
2021-10-09 22:04:21 -0500

    Edit js to show if X or O won the game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,4 @@
 
-const game = document.querySelector("#game");
 
 window.addEventListener('load', (event) => {
     // Insert code here 
@@ -7,7 +6,7 @@ window.addEventListener('load', (event) => {
 let board= document.getElementById('board'); 
 console.log(board);
 let div= board.childNodes;
-console.log(div);
+
 
 
 
@@ -21,7 +20,8 @@ function changeClass(){
 
 
 
-const sq = document.querySelectorAll('.square');
+const sq = document.querySelectorAll('.square');//Array for all the div with class square
+
 
 
 let c=0;
@@ -33,6 +33,7 @@ for (let i = 0; i < sq.length; i++){
         
         e.target.classList.add("X");
         sq[i].textContent="X";
+        winner();
         c++;
           
         }
@@ -42,6 +43,7 @@ for (let i = 0; i < sq.length; i++){
         e.target.classList.add("O");
         sq[i].textContent="O";
         c++;
+        winner();
 }
     
 }
@@ -64,6 +66,76 @@ sq.forEach(function(elem) {
       e.target.classList.remove('hover');
     });
   });
+
+  console.log(sq);
   
+
+
+  const winner_combination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+function winner(){
+
+    let x='';
+    let y='';
+    let z='';
+    
+    winner_combination.forEach((c)=>{
+        if (sq[c[0]].hasChildNodes()){
+        
+        x=sq[c[0]].childNodes[0].textContent;
+        
+        
+
+        }
+        else{
+            x='';
+        }
+        if (sq[c[1]].hasChildNodes()){
+        
+            y=sq[c[1]].childNodes[0].textContent;
+            
+
+        }
+        else{
+            y='';
+        }
+        if (sq[c[2]].hasChildNodes()){
+            z=sq[c[2]].childNodes[0].textContent;
+            
+        }
+        else{
+            z='';
+        }
+        if(z!== '' && y!== '' && x!=='')
+            {
+               
+                if (x== y && x== z){
+                    
+                    
+                    let w = document.getElementById('status');//Notification for winner
+                    w.textContent = `Congratulations! ${x} is the Winner!`
+                    w.classList.add("you-won");
+
+        }
+    }
+    
+    }
+
+                
+    );
+    
+
+}
+
 
 });
